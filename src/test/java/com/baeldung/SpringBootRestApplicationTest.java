f2eb3740e36bb4015e8cf8d98139e85172d634ba
Mehran <36104315+Mehran-at@users.noreply.github.com>
2020-11-15 21:31:00 +0100

    fixing bean bug error

---
+++ src/test/java/com/baeldung/SpringBootRestApplicationTest.java
@@ -0,0 +1,21 @@
+package com.baeldung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {SpringBootRestApplication.class})
+ public class SpringBootRestApplicationTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void modelMapper() {
+    }
+}
